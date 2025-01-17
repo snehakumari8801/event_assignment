a6fd55c7530809f1ec32b3b88ae3f01ae285e180
snehakumari8801 <snehakumari8801@gmail.com>
2025-01-17 15:27:01 +0530

    update register

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,27 +1,23 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Login = () => {
- // let API_BASE_URL = "http://localhost:3000/api/v1"
- let API_BASE_URL = 'https://event-assignment-backend.onrender.com'
+  // let API_BASE_URL = "http://localhost:3000/api/v1"
+  let API_BASE_URL = "https://event-assignment-backend.onrender.com";
 
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
-  const [loading,setLoading] = useState(false);
-
-
- 
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!email || !password) {
-      setError('Please fill in all fields');
+      setError("Please fill in all fields");
       return;
     }
 
@@ -29,41 +25,47 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await axios.post(`${API_BASE_URL}/api/v1/auth/login`, 
-      { email, password });
-      
-      console.log(response.data.user);
-      
-      // Save the JWT token in localStorage or global state
-      localStorage.setItem('token', JSON.stringify(response.data.token));
-      localStorage.setItem('user', JSON.stringify(response.data.user));
+      const response = await axios.post(`${API_BASE_URL}/api/v1/auth/login`, {
+        email,
+        password,
+      });
 
+      console.log(response.data.user);
+
+      // Save the JWT token in localStorage or global state
+      localStorage.setItem("token", JSON.stringify(response.data.token));
+      localStorage.setItem("user", JSON.stringify(response.data.user));
 
       // Redirect to dashboard after successful login
-      navigate('/dashboard');
+      navigate("/dashboard");
       setLoading(false);
     } catch (err) {
       console.log(err);
-      
-      setError('Invalid credentials');
+
+      setError("Invalid credentials");
     }
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300">
-     {/* <Link to="/">
+      {/* <Link to="/">
         <div className=" bg-red-800 text-center pt-2 text-white rounded-xl h-10 
         w-[380px] ">
          <span>Go To Dashboard</span>
         </div>
       </Link> */}
       <div className="w-full max-w-sm p-8 mt-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-3xl font-semibold text-center text-gray-700 mb-6">Login</h2>
+        <h2 className="text-3xl font-semibold text-center text-gray-700 mb-6">
+          Login
+        </h2>
         {error && <div className="text-red-500 text-center mb-4">{error}</div>}
 
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-600 font-medium mb-2">
+            <label
+              htmlFor="email"
+              className="block text-gray-600 font-medium mb-2"
+            >
               Email
             </label>
             <input
@@ -78,7 +80,10 @@ const Login = () => {
           </div>
 
           <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-600 font-medium mb-2">
+            <label
+              htmlFor="password"
+              className="block text-gray-600 font-medium mb-2"
+            >
               Password
             </label>
             <input
@@ -100,17 +105,16 @@ const Login = () => {
           </button>
         </form>
 
-        {loading && (
-          <div className="text-center text-gray-500">Loading...</div>
-        )}
+        {loading && <div className="text-center text-gray-500">Loading...</div>}
 
-
-        <p className="mt-4 text-center text-gray-600">
-          Don't have an account?{' '}
-          <a href="/register" className="text-red-600 hover:underline">
-            Register here
-          </a>
-        </p>
+        <Link to="/register">
+          <p className="mt-4 text-center text-gray-600">
+            Don't have an account?{" "}
+            <a href="/register" className="text-red-600 hover:underline">
+              Register here
+            </a>
+          </p>
+        </Link>
       </div>
     </div>
   );
